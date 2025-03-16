494bb46a983b847e30cf960ca50cdf6d185aa284
sisadul <abdulazizsaepurohmat1@gmail.com>
2025-03-16 17:17:10 +0700

    menambahkan class DataGeneric

--- mod5_103022300092/Program.cs
+++ mod5_103022300092/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric gen = new HaloGeneric();
+        String n = Console.ReadLine();
+        gen.SapaUser(n);
+
+        DataGeneric<string> dat = new DataGeneric<string>("103022300092");
+        dat.PrintData();
+    }
+}
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo {user}");
+    }
+}
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData() 
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
